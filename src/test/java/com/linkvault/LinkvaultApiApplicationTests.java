151a585f4c0fd2161922db3722f1c9b6c7cfd546
ebotsupreme <ejung716@gmail.com>
2025-07-07 08:38:53 -0700

    Initial Spring Boot project setup with .gitignore.

---
+++ src/test/java/com/linkvault/LinkvaultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkvaultApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
