b30ea4ac11adadbebefb6c24c91f85577c206600
ebotsupreme <ejung716@gmail.com>
2025-07-21 13:12:43 -0700

    Add try catch for authController and return success string or fail string.

--- src/main/java/com/linkvault/controller/AuthController.java
+++ src/main/java/com/linkvault/controller/AuthController.java
@@ -4,9 +4,12 @@ import com.linkvault.constants.apiPaths.AuthEndpoints;
 import com.linkvault.dto.LoginRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,13 +30,21 @@ public class AuthController {
     }
 
     @PostMapping(AuthEndpoints.LOGIN)
-    public void login(@Valid @RequestBody LoginRequest loginRequest) {
-        info(log, "Logging in user: {}", loginRequest.getUsername());
-        Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                loginRequest.getUsername(),
-                loginRequest.getPassword()
-            )
-        );
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+        try {
+            info(log, "Logging in user: {}", loginRequest.getUsername());
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    loginRequest.getUsername(),
+                    loginRequest.getPassword()
+                )
+            );
+
+            info(log, "Login successful for user: {}", loginRequest.getUsername());
+            return ResponseEntity.ok("Login successful");
+        } catch (AuthenticationException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+
     }
 }
