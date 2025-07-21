94ed14681dfd9bb0e7c197fa846a912deca8320d
ebotsupreme <ejung716@gmail.com>
2025-07-21 13:00:28 -0700

    Create UserDetailsService and hardcode user and password for testing purposes in SecurityConfig. Auth controller now uses authentication manager to get ready for token generation.

--- src/main/java/com/linkvault/config/SecurityConfig.java
+++ src/main/java/com/linkvault/config/SecurityConfig.java
@@ -2,17 +2,40 @@ package com.linkvault.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    // TODO Remove in Prod
+
+    @Bean
+    public UserDetailsService userDetailService(PasswordEncoder passwordEncoder) {
+        var user = User.withUsername("user")
+            .password(passwordEncoder.encode("password"))
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/login").permitAll()
                 .anyRequest().authenticated()
@@ -20,5 +43,10 @@ public class SecurityConfig {
             .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
     }
 }
--- src/main/java/com/linkvault/controller/AuthController.java
+++ src/main/java/com/linkvault/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.linkvault.constants.apiPaths.AuthEndpoints;
 import com.linkvault.dto.LoginRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,9 +20,20 @@ import static com.linkvault.util.LogUtils.info;
 @RestController
 @RequestMapping(AuthEndpoints.BASE_AUTH)
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
 
     @PostMapping(AuthEndpoints.LOGIN)
     public void login(@Valid @RequestBody LoginRequest loginRequest) {
         info(log, "Logging in user: {}", loginRequest.getUsername());
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword()
+            )
+        );
     }
 }
