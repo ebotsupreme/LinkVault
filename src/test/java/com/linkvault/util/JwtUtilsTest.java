65c0e89583af49d555321c87f18f86ad35dd3b55
ebotsupreme <ejung716@gmail.com>
2025-07-22 13:00:57 -0700

    Create JwtUtilsTest

---
+++ src/test/java/com/linkvault/util/JwtUtilsTest.java
@@ -0,0 +1,41 @@
+package com.linkvault.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JwtUtilsTest {
+    private JwtUtils jwtUtils;
+    private String token;
+    private static final String TEST_USER = "testuser";
+
+    @BeforeEach
+    void setUp(){
+        String testSecret = "sxy8aEjMvcnPANTL/AuUGOGWUrZl4w2JeP+ztrVca24=";
+        // 1 hour
+        long testExpiration = 1000L * 60 * 60;
+        jwtUtils = new JwtUtils(testSecret, testExpiration);
+        this.token = jwtUtils.generateToken(TEST_USER);
+    }
+
+    @Test
+    void generateToken_ShouldReturnToken() {
+        assertNotNull(token);
+    }
+
+    @Test
+    void generateToken_ShouldReturnTrue_ForValidToken() {
+        assertTrue(jwtUtils.validateToken(token));
+    }
+
+    @Test
+    void generateToken_ShouldReturnFalse_ForInvalidToken() {
+        assertFalse(jwtUtils.validateToken("invalid.token.here"));
+    }
+
+    @Test
+    void extractUsername_ShouldReturnCorrectUsername() {
+        assertEquals(TEST_USER, jwtUtils.extractUsername(token));
+    }
+}
